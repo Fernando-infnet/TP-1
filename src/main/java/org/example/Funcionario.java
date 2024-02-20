eadea71ea09cb30b74580daee2a61998b61801e1
https://github.com/lntra <fernandoaraujo1202@gmail.com>
2024-02-19 21:07:57 -0300

    Criar projeto

--- src/main/java/org/example/Funcionario.java
+++ src/main/java/org/example/Funcionario.java
@@ -1,42 +1,57 @@
 package org.example;
 
+import java.util.Objects;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Funcionario {
-    private String nome;
     private String area;
-    private double salario;
     private String funcao;
 
+    private String nome;
+
+    private double salario;
+
     public Funcionario(String area, String nome, String funcao, double salario) {
-        this.area = area;
-        this.funcao = funcao;
-        this.nome = nome;
-        this.salario = salario;
+        if(!Objects.equals(area, "")){
+            this.area = area;
+        }
+        else {
+            throw new IllegalArgumentException("Área não pode ser nulo");
+        }
+        if(!Objects.equals(funcao, "")){
+            this.funcao = funcao;
+        }
+        else {
+            throw new IllegalArgumentException("Função não pode ser nulo");
+        }
+        if(!Objects.equals(nome, "")){
+            this.nome = nome;
+        }
+        else {
+            throw new IllegalArgumentException("Nome não pode ser nulo");
+        }
+        if(salario > 0){
+            this.salario = salario;
+
+        }
+        else {
+            throw new IllegalArgumentException("Salário tem de ser maior que 0");
+        }
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
     public String getArea() {
         return area;
     }
 
     public void setArea(String area) {
-        this.area = area;
-    }
-
-    public double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(double salario) {
-        this.salario = salario;
+        if(!Objects.equals(area, "")){
+            this.area = area;
+        }
+        else {
+            throw new IllegalArgumentException("Área não pode ser nulo");
+        }
     }
 
     public String getFuncao() {
@@ -44,11 +59,48 @@ public class Funcionario {
     }
 
     public void setfuncao(String funcao) {
-        this.funcao = funcao;
+        if(!Objects.equals(funcao, "")){
+            this.funcao = funcao;
+        }
+        else {
+            throw new IllegalArgumentException("Função não pode ser nulo");
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if(!Objects.equals(nome, "")){
+            this.nome = nome;
+        }
+        else {
+            throw new IllegalArgumentException("Nome não pode ser nulo");
+        }
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        if(salario > 0){
+            this.salario = salario;
+
+        }
+        else {
+            throw new IllegalArgumentException("Salário tem de ser maior que 0");
+        }
     }
 
     public void aplicarAumento(double percentualAumento) {
-        double aumento = salario * percentualAumento / 100;
-        salario += aumento;
+        if(percentualAumento > 0){
+            double aumento = salario * percentualAumento / 100;
+            salario += aumento;
+        }
+        else {
+            throw new IllegalArgumentException("Salário tem de ser maior que 0");
+        }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,32 +3,61 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // Exemplos funcionários incompatíveis
-        Funcionario funcionario1 = new Funcionario("Sétimo andar, 402", "Gabriel Souza", "Engenheiro de Software", "2000");
+            /*
+                Funcionario funcionario3 = new Funcionario("Sétimo andar, 702", "Gabriel Souza", "Engenheiro de Software", "2000");
+                Funcionario funcionario4 = new Funcionario(984, "Gabriel Souza", "Engenheiro de Software", 2000);
+                Funcionario funcionario5 = new Funcionario("Sétimo andar, 702", GabrielSouza, "Engenheiro de Software", 2000);
+                Funcionario funcionario6 = new Funcionario("Sétimo andar, 702", "Gabriel Souza", 23, "2000");
+            */
         // Exemplo funionário funcional
-        Funcionario funcionario2 = new Funcionario("João Silva", "Analista de TI", 5000.0, "TI");
-
-        // Exemplo de chamada de métodos para manipular o objeto
-        System.out.println("Salário antes do aumento: " + funcionario1.getSalario());
-        funcionario1.aplicarAumento(10); // Aumento de 10%
-        System.out.println("Salário após o aumento: " + funcionario1.getSalario());
-
-        // Alteração do cargo do funcionário
-        funcionario1.setCargo("Gerente de TI");
-
-        // Exemplo de criação de outro funcionário com diferentes valores de atributos
-        Funcionario funcionario3 = new Funcionario("Maria Oliveira", "Analista Financeiro", 6000.0, "Financeiro");
+        Funcionario funcionario1 = new Funcionario("Sétimo andar, 703", "Gabriel Souza", "Engenheiro de Software", 2000);
+        Funcionario funcionario2 = new Funcionario("Quarto andar, 401", "Fernando Machado", "Engenheiro de Software", 3200);
 
         // Impressão dos detalhes dos funcionários
         System.out.println("Detalhes do Funcionário 1:");
         System.out.println("Nome: " + funcionario1.getNome());
-        System.out.println("Cargo: " + funcionario1.getCargo());
+        System.out.println("Área: " + funcionario1.getArea());
         System.out.println("Salário: " + funcionario1.getSalario());
-        System.out.println("Departamento: " + funcionario1.getDepartamento());
+        System.out.println("Função: " + funcionario1.getFuncao());
 
         System.out.println("\nDetalhes do Funcionário 2:");
         System.out.println("Nome: " + funcionario2.getNome());
-        System.out.println("Cargo: " + funcionario2.getCargo());
+        System.out.println("Área: " + funcionario2.getArea());
         System.out.println("Salário: " + funcionario2.getSalario());
-        System.out.println("Departamento: " + funcionario2.getDepartamento());
+        System.out.println("Função: " + funcionario2.getFuncao());
+
+        funcionario2.setNome("Fernando Maia");
+        funcionario2.setfuncao("Cientista de dados");
+
+        System.out.println("\nDetalhes do Funcionário 2:");
+        System.out.println("Nome: " + funcionario2.getNome());
+        System.out.println("Área: " + funcionario2.getArea());
+        System.out.println("Salário: " + funcionario2.getSalario());
+        System.out.println("Função: " + funcionario2.getFuncao());
+
+        /* testar erros
+        funcionario2.setNome("");
+
+        System.out.println("\nDetalhes do Funcionário 2:");
+        System.out.println("Nome: " + funcionario2.getNome());
+        System.out.println("Área: " + funcionario2.getArea());
+        System.out.println("Salário: " + funcionario2.getSalario());
+        System.out.println("Função: " + funcionario2.getFuncao());
+
+        funcionario2.setSalario(0);
+
+        System.out.println("\nDetalhes do Funcionário 2:");
+        System.out.println("Nome: " + funcionario2.getNome());
+        System.out.println("Área: " + funcionario2.getArea());
+        System.out.println("Salário: " + funcionario2.getSalario());
+        System.out.println("Função: " + funcionario2.getFuncao());
+
+         */
+
+        // Exemplo de chamada de métodos para manipular o objeto
+        System.out.println("\nSalário de " + funcionario1.getNome() + " antes do aumento: " + funcionario1.getSalario());
+        funcionario1.aplicarAumento(10); // Aumento de 10%
+        System.out.println("Salário de " + funcionario1.getNome() + " após o aumento: " + funcionario1.getSalario());
+
     }
 }
